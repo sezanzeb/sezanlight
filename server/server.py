10f0453a3c33f18e55ad22b5974ba1f190b7b325
sezanzeb <proxima@hip70890b.de>
2018-12-25 18:38:10 +0100

    attempt 1 on smooth fading; bugfix 1

--- server/server.py
+++ server/server.py
@@ -23,15 +23,20 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         url = self.path # /?r=128&g=128&b=128
         color_strings = re.split('[^\d]+', url)[1:]
 
-        r_new = r
-        g_new = g
-        b_new = b
+        r_new = int(color_strings[0])
+        g_new = int(color_strings[1])
+        b_new = int(color_strings[2])
 
+        # smoothly fade. after 1 second this loop should be ended,
+        # therefore i end after 59/60th of a second
         for i in range(59):
             f = i/59
-            pi.set_PWM_dutycycle(17, r*(1-f) + r_new*(f))
-            pi.set_PWM_dutycycle(22, g*(1-f) + g_new*(f))
-            pi.set_PWM_dutycycle(24, b*(1-f) + b_new*(f))
+            r_fade = int(r*(1-f) + r_new*(f))
+            g_fade = int(g*(1-f) + g_new*(f))
+            b_fade = int(b*(1-f) + b_new*(f))
+            pi.set_PWM_dutycycle(17, r_fade)
+            pi.set_PWM_dutycycle(22, g_fade)
+            pi.set_PWM_dutycycle(24, b_fade)
             time.sleep(1/60)
 
         pi.set_PWM_dutycycle(17, r)
