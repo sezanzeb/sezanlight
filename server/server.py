9509410bf9ddb4ab547d0a0d3cd05635636fc533
sezanzeb <proxima@hip70890b.de>
2018-12-28 05:57:09 +0100

    a million times better server architecture. Should handle irregular requests quite well now

--- server/server.py
+++ server/server.py
@@ -1,20 +1,55 @@
 #!/usr/bin/env python3
 
+"""
+    this server consists of two threads:
+    the main thread:
+        listens for get requests and decodes the colors from the get params (stored in url)
+        then overwrites the global variables
+    the fader thread:
+        the overwritten global variables, overwritten by the main thread, are those variables
+        that control the fading. The fader is in an infinite loop that just fades from the color
+        from the state when a request was made to the color of the request. This way, when multiple
+        requests are made within a short amount of time, the previous fading task is basically stopped,
+        it takes the color that it stopped at and fades to the new color.
+"""
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import re
 import pigpio
 import time
-# from multiprocessing import Process
+from threading import Thread
 
 pi = pigpio.pi()
 
-# the fading process
-# proc = None
+# the fadpything thread
+thread = None
 
+# current color during fading and statically
 r = 0
 g = 0
 b = 0
 
+# color which is used as the fading starting color
+r_start = 0
+g_start = 0
+b_start = 0
+
+# color to fade into
+r_target = 0
+g_target = 0
+b_target = 0
+
+# how much progress the current fading already has
+f = 0
+
+# fading config
+frequency = 200 #hz
+
+# going to be overwritten during get request:
+checks_per_second = 1
+checks = 0
+
+# hardware setup
 gpio_r = 17
 gpio_g = 22
 gpio_b = 24
@@ -28,73 +63,86 @@ pi.set_PWM_range(gpio_r, full_on)
 pi.set_PWM_range(gpio_g, full_on)
 pi.set_PWM_range(gpio_b, full_on)
 
-def fade(r, g, b, r_new, g_new, b_new, checks_per_second):
+def fade():
 
-    # smoothly fade. after 1 second (depending on checks_per_second)
-    # this loop should be ended and a new one started
-    frequency = 200 #hz
-    checks = int(frequency/checks_per_second)-1
+    # going to overwrite those globally
+    global r, g, b, f
 
-    for i in range(checks):
-        # f will move from 0 to 1
+    while True:
         start = time.time()
-        f = i/checks
-        # and add old and new color together proportionally
-        # so that a fading effect is created
-        r_fade = int(r*(1-f) + r_new*(f))
-        g_fade = int(g*(1-f) + g_new*(f))
-        b_fade = int(b*(1-f) + b_new*(f))
-        pi.set_PWM_dutycycle(17, r_fade)
-        pi.set_PWM_dutycycle(22, g_fade)
-        pi.set_PWM_dutycycle(24, b_fade)
+        # f will move from 0 to 1
+        if checks > 1:
+            f += 1/checks
+
+            if f < 1:
+                # Add old and new color together proportionally
+                # so that a fading effect is created.
+                # Overwrite globals r, g and b so that when fading restarts,
+                # that is going to be the new starting color.
+                r = int(r_start*(1-f) + r_target*(f))
+                g = int(g_start*(1-f) + g_target*(f))
+                b = int(b_start*(1-f) + b_target*(f))
+                pi.set_PWM_dutycycle(gpio_r, r)
+                pi.set_PWM_dutycycle(gpio_g, g)
+                pi.set_PWM_dutycycle(gpio_b, b)  
+                print(r, g, b)
+
+            else:
+                pi.set_PWM_dutycycle(gpio_r, r_target)
+                pi.set_PWM_dutycycle(gpio_g, g_target)
+                pi.set_PWM_dutycycle(gpio_b, b_target)
+                pass
+
         delta = time.time()-start
         time.sleep(max(0, 1/checks_per_second/(checks+1)-delta))
 
-    pi.set_PWM_dutycycle(gpio_r, r_new)
-    pi.set_PWM_dutycycle(gpio_g, g_new)
-    pi.set_PWM_dutycycle(gpio_b, b_new)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        global r, g, b, proc
+        global r, g, b
+        global checks_per_second, checks, f
+        global r_target, g_target, b_target
+        global r_start, g_start, b_start
 
         url = self.path # /?r=128&g=128&b=128
 
         # quickly send ok, don't block the client
-        # (still blocks the client)
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'1')
-
         # https://stackoverflow.com/questions/6594418/simplehttprequesthandler-close-connection-before-returning-from-do-post-method
         # self.finish()
-        self.connection.close()
+        # self.connection.close()
+        # doesnt work...
+        # (still blocks the client)
 
         params = re.split('[^\d]+', url)[1:]
 
-        r_new = int(params[0])
-        g_new = int(params[1])
-        b_new = int(params[2])
         checks_per_second = int(params[3])
+        checks = int(frequency/checks_per_second)-1
 
-        """# fade the color in a new process so that the client is not blocked
-        # make sure two fading processes are not overlapping
-        if proc is None or not proc.is_alive():
-            proc = Process(target=fade, args=(r, g, b, r_new, g_new, b_new, checks_per_second))
-            proc.start()
+        # restart fading:
+        f = 0
+        r_target = int(params[0])
+        g_target = int(params[1])
+        b_target = int(params[2])
+        # start where fading has just been
+        r_start = r
+        g_start = g
+        b_start = b
 
-            # overwrite state of server
-            r = r_new
-            g = g_new
-            b = b_new"""
-
-        fade(r, g, b, r_new, g_new, b_new, checks_per_second)
-        r = r_new
-        g = g_new
-        b = b_new
+# the thread that just keeps fading forever,
+# whereas the main thread writes the variables from the get request into the
+# processes (shared between threads) memory that are used for fading:
+fader = Thread(target=fade)
+fader.start()
 
 print('listening on', raspberry_ip + ':' + str(raspberry_port))
 httpd = HTTPServer((raspberry_ip, raspberry_port), SimpleHTTPRequestHandler)
 httpd.serve_forever()
+
+
+
+
